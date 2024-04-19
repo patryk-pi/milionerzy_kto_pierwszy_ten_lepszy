272ddbd29d3cc60e8ed6bb6fc4d0c1a8c5620df0
patryk-pi <patryk.piasecki1@gmail.com>
2024-04-19 07:56:34 +0200

    added Global styles for styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import Answer from "./components/Answer";
 import { StyledContainer } from "./components/styles/Container.styled";
+import GlobalStyles from "./components/styles/Global";
 
 function App() {
     const answers: string[] = ["A", "B", "C", "D"];
 
     return (
-        <StyledContainer>
-            <div>
-                {answers.map((answer, index) => (
-                    <Answer key={index} answer={answer} />
-                ))}
-            </div>
-        </StyledContainer>
+        <>
+            <GlobalStyles />
+            <StyledContainer>
+                <div>
+                    {answers.map((answer, index) => (
+                        <Answer key={index} answer={answer} />
+                    ))}
+                </div>
+            </StyledContainer>
+        </>
     );
 }
 
--- src/components/styles/Container.styled.ts
+++ src/components/styles/Container.styled.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-    width: 350px;
     margin: 0 auto;
+    box-sizing: border-box;
 `;
