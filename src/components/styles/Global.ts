19430d64282b3ca5e4efe1078e5eb4991161cb00
patryk-pi <patryk.piasecki1@gmail.com>
2024-04-19 14:52:24 +0200

    changed height of the main container

--- src/components/styles/Global.ts
+++ src/components/styles/Global.ts
@@ -10,7 +10,7 @@ const GlobalStyles = createGlobalStyle`
        
         font-family: Arial, sans-serif;
         margin: 0;
-        height: 100vh;
+        height: 120vh;
         background: rgb(2,0,36);
         background: linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,97,255,1) 100%);
         background-repeat: no-repeat;
